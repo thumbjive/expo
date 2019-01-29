7521d3a6fbff093b0d7630b55b89b23b4d657844
Stanisław Chmiela <sjchmiela@gmail.com>
2019-01-29 12:22:09 +0100

    [expo-analytics-amplitude] Move TS code to expo-analytics-amplitude

--- packages/expo-analytics-amplitude/src/__tests__/Amplitude-test.ts
+++ packages/expo-analytics-amplitude/src/__tests__/Amplitude-test.ts
@@ -1,38 +1,40 @@
 import { describeCrossPlatform } from 'jest-expo';
-import { NativeModules } from 'react-native';
-import Amplitude from '../Amplitude/Amplitude';
+import { NativeModulesProxy } from 'expo-core';
+import * as Amplitude from '../Amplitude';
+
+const { ExpoAmplitude } = NativeModulesProxy;
 
 describeCrossPlatform('all Amplitude methods available', () => {
   it('initializes', () => {
     expect(Amplitude.initialize('test-api-key')).toBeUndefined();
-    expect(NativeModules.ExponentAmplitude.initialize).toHaveBeenCalledTimes(1);
+    expect(ExpoAmplitude.initialize).toHaveBeenCalledTimes(1);
   });
 
   it('can setUserId', () => {
     expect(Amplitude.setUserId('user-id')).toBeUndefined();
-    expect(NativeModules.ExponentAmplitude.setUserId).toHaveBeenCalledWith('user-id');
+    expect(ExpoAmplitude.setUserId).toHaveBeenCalledWith('user-id');
   });
 
   it('can setUserProperties', () => {
     expect(Amplitude.setUserProperties({ some: 'property' })).toBeUndefined();
-    expect(NativeModules.ExponentAmplitude.setUserProperties).toHaveBeenCalledWith({
+    expect(ExpoAmplitude.setUserProperties).toHaveBeenCalledWith({
       some: 'property',
     });
   });
 
   it('can clearUserProperties', () => {
     expect(Amplitude.clearUserProperties()).toBeUndefined();
-    expect(NativeModules.ExponentAmplitude.clearUserProperties).toHaveBeenCalledTimes(1);
+    expect(ExpoAmplitude.clearUserProperties).toHaveBeenCalledTimes(1);
   });
 
   it('can logEvent', () => {
     expect(Amplitude.logEvent('event-name')).toBeUndefined();
-    expect(NativeModules.ExponentAmplitude.logEvent).toHaveBeenCalledWith('event-name');
+    expect(ExpoAmplitude.logEvent).toHaveBeenCalledWith('event-name');
   });
 
   it('can logEventWithProperties', () => {
     expect(Amplitude.logEventWithProperties('event-name', { some: 'property' })).toBeUndefined();
-    expect(NativeModules.ExponentAmplitude.logEventWithProperties).toHaveBeenCalledWith(
+    expect(ExpoAmplitude.logEventWithProperties).toHaveBeenCalledWith(
       'event-name',
       { some: 'property' }
     );
@@ -40,7 +42,7 @@ describeCrossPlatform('all Amplitude methods available', () => {
 
   it('can setGroup', () => {
     expect(Amplitude.setGroup('group', ['group', 'names', 'array'])).toBeUndefined();
-    expect(NativeModules.ExponentAmplitude.setGroup).toHaveBeenCalledWith('group', [
+    expect(ExpoAmplitude.setGroup).toHaveBeenCalledWith('group', [
       'group',
       'names',
       'array',
